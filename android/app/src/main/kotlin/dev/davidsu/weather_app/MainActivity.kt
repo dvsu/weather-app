8930bd073155537bea5c4e7e51d08edecf219cc2
David S <dave.techdev@gmail.com>
2021-06-20 21:42:43 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/dev/davidsu/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.davidsu.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
